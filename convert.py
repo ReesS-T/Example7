c5e6a4842ec57e677f70ba6814aea7f92df1d06e
@ReesS-T <rsweeneytaylor@hks.harvard.edu>
2020-01-16 14:24:25 -0500

    Converted dates from input.csv into a usable form

--- convert.py
+++ convert.py
@@ -1,21 +1,33 @@
 #!/usr/bin/env python3
 
 import csv
-import datetime
+from datetime import datetime
 
 # 1. load input.csv into a variable called `polls`
-
+with open('input.csv') as f:
+	reader = csv.DictReader(f)
+	rows =list(reader)
+	polls = [dict(row) for row in rows]
+   
 # 2. write a new file called output.csv and write a row with two headers: "date" and "approve"
+with open('output.csv', "w") as f:
+	writer = csv.writer(f)
+	writer.writerow(['date', 'approval'])
 
-# 3. Loop through each row of `polls` 
 
-    # 4. and within that loop... convert the format of `enddate` from "1/22/2017" to "22-Jan-17"
-    # hint: to read the date you will need to use
-    #       date = datetime.datetime.strptime(myrawstring, "insert input format here")
-    #
-    #       and to write the date you will need to use something like 
-    #       new_date = date.strftime("insert output format here")
-    # 
-    #       date formats can be found at https://strftime.org/
-    
-    # 5. write a new row of data with the transformed date and value for "approve" 
+#	3. Loop through each row of `polls` 
+	for p in polls:
+		raw_date = p["enddate"]
+		approve= p['approve']
+
+# 4. and within that loop... convert the format of `enddate` from "1/22/2017" to "22-Jan-17"
+		date_format = "%m/%d/%Y"
+		parsed_date = datetime.strptime(raw_date, date_format)
+		date_str = parsed_date.strftime("%d-%b-%y")
+
+# 5. write a new row of data with the transformed date and value for "approve" 
+		writer.writerow([date_str, approve])
+
+
+	
+#By Rees Sweeney-Taylor, Alina Cough, Adam Staveski
